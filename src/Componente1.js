f03cede5bcc621abfd0dbf49bbc61498e0960687
Vito Millozza <v.millozza@gmail.com>
2023-08-28 14:51:40 +0200

    Innested Components

--- src/Componente1.js
+++ src/Componente1.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
 const Componente1 = () => {
-  return (
-    <div>
-      <h1>Componente1</h1>
-    </div>
-  )
-}
+    return (
+        <React.Fragment>
+          
+          <Saluto></Saluto>
 
+        </React.Fragment>
+    )
+}
+const Persona = ()=>{
+  return <h2>Mi chiamo Vito</h2>;
+}
+const Messaggio =()=>{ 
+    return <h1>Sono un messaggio</h1>;
+
+
+}
+const Saluto=()=>{
+    return  <React.Fragment>
+    <section>
+        <Persona></Persona>
+        <Messaggio></Messaggio>
+    </section>
+
+</React.Fragment>;
+}
 export default Componente1
